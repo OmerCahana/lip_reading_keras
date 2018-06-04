1aaf8a6f07259acad5995cd283c88e31e9c7214a
Omer cahana <omer4436@gmail.com>
2018-06-04 17:30:01 +0300

    Add files via upload

--- LipReading_keras/Data/Preprocessing.py
+++ LipReading_keras/Data/Preprocessing.py
@@ -1,23 +1,17 @@
 import os
 import dlib
-from imutils.video import FileVideoStream
 from imutils import face_utils
-import matplotlib.pyplot as plt
-from skimage import io
 import cv2
-from PIL import Image, ImageOps
 import numpy as np
 import glob
-from scipy import ndimage
-import random
-import math
-from sklearn.utils import shuffle
 import h5py
 import time
 from collections import OrderedDict
 
 ### You should download shape_predictor_68_face_landmarks.dat
 ### from http://dlib.net/files/
+
+
 
 def get_labels(directory = 'lipread_mp4'):
     ''' 
@@ -28,67 +22,14 @@ def get_labels(directory = 'lipread_mp4'):
     Will be used for y_train, y_val, y_test
     '''
     labels = {}
-    temp = {}
     idx = 0
-    
+
     for name in os.listdir(directory):
         labels[name] = idx
         idx += 1
-    
+
     labels = list(OrderedDict(sorted(labels.items(), key=lambda t: t[1])).items())
-    return labels 
-
-
-
-
-def AT_process(image, angle):
-    ''' 
-    Most of the function are from Augmentator library: 
-    https://github.com/mdbloice/Augmentor/blob/master/Augmentor/Operations.py
-    class RotateRange which is based upod PIL.Image
-
-    Returns rotated 20 degrees image without black area for AUGMENTATION purposes
-    '''
-    image = Image.fromarray(image)
-    x = image.size[0]
-    y = image.size[1]
-
-    # Rotate, while expanding the canvas size
-    image = image.rotate(angle, expand=True, resample=Image.BICUBIC)
-    
-    # Get size after rotation, which includes the empty space
-    X = image.size[0]
-    Y = image.size[1]
-
-    # Get our two angles needed for the calculation of the largest area
-    angle_a = abs(angle)
-    angle_b = 90 - angle_a
-
-    # Python deals in radians so get our radians
-    angle_a_rad = math.radians(angle_a)
-    angle_b_rad = math.radians(angle_b)
-
-    # Calculate the sins
-    angle_a_sin = math.sin(angle_a_rad)
-    angle_b_sin = math.sin(angle_b_rad)
-
-    # Find the maximum area of the rectangle that could be cropped
-    E = (math.sin(angle_a_rad)) / (math.sin(angle_b_rad)) * \
-        (Y - X * (math.sin(angle_a_rad) / math.sin(angle_b_rad)))
-    E = E / 1 - (math.sin(angle_a_rad) ** 2 / math.sin(angle_b_rad) ** 2)
-    B = X - E
-    A = (math.sin(angle_a_rad) / math.sin(angle_b_rad)) * B
-
-    # Crop this area from the rotated image
-    # image = image.crop((E, A, X - E, Y - A)), changed some values 
-    image = image.crop((int(round(E)), int(round(A)-5), int(round(X - E+10)), int(round(Y - A +10))))
-
-    # Return the image, re-sized to the size of the image passed originally
-    image = image.resize((x, y), resample=Image.BICUBIC)
-    return np.array(image)
-
-
-
+    return labels
 
 def norm_(array, mean = 0.4161, std = 0.1688,with_std = False):
     ''' 
@@ -101,23 +42,6 @@ def norm_(array, mean = 0.4161, std = 0.1688,with_std = False):
         array = array/std
     return array
 
-
-
-
-def shuffle_(array_x, array_y):
-    ''' 
-    Symmetrically shuffle 2 arrays using random lib and sklearn.utils
-    '''
-    N = array_x.shape[0]
-    state = np.random.randint(1,N)
-    
-    shuffle(array_x, random_state = state)
-    shuffle(array_y, random_state = state)
-    return (array_x, array_y)
-
-
-
-
 def hdf5_saver(x_array, y_array, hdf5_path, x_name, y_name):
     '''
     Save you preprocessed array for further use
@@ -127,11 +51,11 @@ def hdf5_saver(x_array, y_array, hdf5_path, x_name, y_name):
     '''
     hdf5_file = h5py.File(hdf5_path, mode='w')
     N = x_array.shape[0]
-    
+
     hdf5_file.create_dataset(x_name, (N,29,112,112,1), np.uint8)
     hdf5_file[x_name][...] = x_array
 
-    hdf5_file.create_dataset(y_name, (N,50) , np.uint16)
+    hdf5_file.create_dataset(y_name, (N,29,1) , np.uint16)
     hdf5_file[y_name][...] = y_array
     hdf5_file.close()
     return print("File succesfully created and saved in %s" % (hdf5_path))
@@ -140,66 +64,62 @@ def hdf5_saver(x_array, y_array, hdf5_path, x_name, y_name):
 ##################################################### MAIN ENGINE ###########################################################
 #############################################################################################################################
 
-def video2data(video, face_detector, landmarks_predictor, AT = False):
-    ''' 
-    Our core detector, which uses 2D FACE ALIGNMENT model based on dlib
-    https://github.com/1adrianb/face-alignment by Bulat, Adrian and Tzimiropoulos, Georgios
+def video2data(filename, face_detector, landmarks_predictor, radius=56):
+    '''
+    Our core detector, which uses dlib Face detector and Face Landmarks predictor
 
-    sleep_time: usually it takes from 0.3 to 2 sec to get frames
+    Args:
+    frames:
+        output of video2frames function (grayscaled [29 x 256 x 256])
+    predictor_path:
+        path to dlib.shape_predictor weights
 
-    AT:   augmentation of transposed images(-20 or 20)
+    Returns:
+        preprocessed video in form of [29 x radius*2 x radius*2]
+        of cropped mouth regions from each frame
+    '''
 
-    Returns: preprocessed video in form of [29 x 112 x 112]
-    of cropped mouth regions from each frame
-    as well as calculates total time taken
-    '''    
-    start = time.time()
-    # GETTING 29 FRAMES using IMUTILS
-    vs = FileVideoStream(video).start()
-    time.sleep(0.1)
-    frames = []
-    
-    while vs.more():
-        frame = vs.read()
-        frames.append(frame)
+    video_cap = cv2.VideoCapture(filename)
+    success, image = video_cap.read()
+    gray = [cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)]
+    while success:
+        success, image = video_cap.read()
+        # print('Read a new frame: ', success)
+        if success:
+            gray.append(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+    frames = np.array(gray)
 
-    #if AT:
-       # angle = random.choice((20,-20))
-    # DETECTING FACES AND CROPPING MOUTH REGION USING 2D FACE ALIGNMENT algorithm   
-    # empty container for further use (for landmarks)
-    lm4video= np.zeros((29,68,2)) 
-
+    lenf = len(frames)
+    lm4video = np.zeros((lenf, 68, 2))
     for i in range(len(frames)):
         img = frames[i]
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        face = face_detector(gray,0)
+        face = face_detector(img, 0)
+
         if not face:
-            return None
-        preds = landmarks_predictor(gray,face[0])
+            center = 128
+            mouth = frames[:, center - radius: center + radius, center - radius: center + radius]
+            return mouth
+
+        preds = landmarks_predictor(img, face[0])
         preds = face_utils.shape_to_np(preds)
-        lm4video[i,:,:] = preds
-            
-    # median coordinates for all 29 video frames in order to get best crop area for all frames
-    # ideal crop: 1/4 of nose, 1/2 of chin, width depends on its size
-    mc = np.median(lm4video,axis=0)
-    y0,y1 = int(np.mean((mc[29][1],mc[30][1]))), int(mc[7][1])
-    center = int(np.mean((mc[52][0], mc[63][0], mc[67][0], mc[58][0])))
-    width_from_center = (y1 - y0)//2
-    x0 = center - width_from_center
-    x1 = center + width_from_center
-    
-    idx = 0
-    f2x = np.zeros((29,112,112))
-    for frame in frames:
-        mouth = frame[y0:y1, x0:x1]
-        mouth = cv2.resize(mouth,(112,112)) 
-        mouth = cv2.cvtColor(mouth, cv2.COLOR_BGR2GRAY)
-        #  Augmentation, rotated images
-        #if AT:
-            #mouth = AT_process(mouth,angle)
-        f2x[idx,:,:] = mouth
-        idx += 1
-    return f2x
+        lm4video[i, :, :] = preds
+
+    ##### 3 steps
+    # 1: find mean coordinates of all mouth landmarks
+    # 2: choose median as center
+    # 3: take an area of 112 x 112 pixels around center
+    mouth = lm4video[:, 48:, :]
+    mc = np.mean(mouth, axis=1)
+    center = np.median(mc, axis=0)
+    y_center = int(center[1])
+    x_center = int(center[0])
+
+    frames = np.array(frames)
+    frames = norm_(frames)
+    mouth = frames[:, y_center - radius: y_center + radius, x_center - radius: x_center + radius]
+
+    return mouth
+
 
 ##############################################################################################################################
 
@@ -220,14 +140,14 @@ class dataset_builder():
                                              x_test, y_test
         
     '''
-    def __init__(self, path, gpu = True):
+    def __init__(self, path):
         self.labels = get_labels(path)
         self.path = path
-        
+
         # init of pretrained model
         self.detector_dlib = dlib.get_frontal_face_detector()
         self.predictor_dlib = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-        
+
     def get_train(self, examples_per_label, label_start_index, AT = False):
         ''' 
         Main functions of class. 
@@ -246,7 +166,7 @@ class dataset_builder():
         start = time.time()
         hdf5_idx = 0
         N = 40000
-        stop = N-1 
+        stop = N-1
         self.x_train= np.zeros((N,29,112,112), dtype = np.uint8)
         self.y_train = np.zeros((N,29,1), dtype = np.uint16)
         i_0 = 0
@@ -261,24 +181,23 @@ class dataset_builder():
                 index = i_0 + ix
                 if type(result) == np.ndarray:
                     self.x_train[index] = result
-                    
-            if index >= stop:
-                hdf5_path = '/home/Q/' + str(hdf5_idx) + 'part.hdf5'
-                hdf5_saver(self.x_train, self.y_train, hdf5_path, 'x_train', 'y_train')
-                hdf5_idx += 1
-                i_0 = 0
-                self.x_train = np.zeros((N,29,112,112), dtype = np.uint8)
-                self.y_train = np.zeros((N,1), dtype = np.uint16)
-                print (start - time.time())
-            i_0 = i_1 
-        
-              
+
+
+            hdf5_path = '/home/usr/train_set.hdf5'
+            hdf5_saver(self.x_train, self.y_train, hdf5_path, 'x_train', 'y_train')
+            hdf5_idx += 1
+            i_0 = 0
+            self.x_train = np.zeros((N,29,112,112), dtype = np.uint8)
+            self.y_train = np.zeros((N,29,1), dtype = np.uint16)
+            print (start - time.time())
+
+
     def get_val(self, examples_per_label, hdf5_path, AT = False):
         ''' 
         SEE API get_train
         examples_per_label maximum value is 50
         hdf5_path example: "/home/USERNAME/val_set.hdf5"
-        '''    
+        '''
         start = time.time()
         N = examples_per_label * 500
         stop = N-1 # last index of each label
@@ -295,20 +214,20 @@ class dataset_builder():
                 result = video2data(video, self.detector_dlib, self.predictor_dlib, False)
                 index = i_0 + ix
                 if type(result) == np.ndarray:
-                    self.x_val[index] = result  
+                    self.x_val[index] = result
             if index >= stop:
                 hdf5_saver(self.x_val, self.y_val, hdf5_path, 'x_val', 'y_val')
                 print (start - time.time())
-            i_0 = i_1 
-         
-        
-        
+            i_0 = i_1
+
+
+
     def get_test(self, examples_per_label, hdf5_path, AT = False):
         ''' 
         SEE API get_train
         examples_per_label maximum value is 50
         hdf5_path example: "/home/USERNAME/test_set.hdf5"
-        '''    
+        '''
         start = time.time()
         N = examples_per_label * 500
         stop = N-1 # last index of each label
@@ -325,9 +244,9 @@ class dataset_builder():
                 result = video2data(video, self.detector_dlib, self.predictor_dlib, False)
                 index = i_0 + ix
                 if type(result) == np.ndarray:
-                    self.x_test[index] = result  
+                    self.x_test[index] = result
             if index >= stop:
                 hdf5_saver(self.x_test, self.y_test, hdf5_path, 'x_test', 'y_test')
-                print (start - time.time())
+                print(start - time.time())
             i_0 = i_1 
       
--- LipReading_keras/README.md
+++ LipReading_keras/README.md
@@ -1,18 +1,18 @@
 # Lipreading With Machine Learning In keras
-A keras implementation of the models described in [Combining Residual Networks with LSTMs for Lipreading]  by T. Stafylakis and G. Tzimiropoulos, with another preprocessing
+A keras implementation of the models described in [Combining Residual Networks with LSTMs for Lipreading]  by T. Stafylakis and G. Tzimiropoulos.
 
 ## Usage
  - Install [Python 3].
  - Clone the repository.
  - Run `pip3 install -r requirements.txt` to install project dependencies.
- - Build the data with `python3 buildDataset.py` 
+ - Build the data with `python3 data/buildDataset.py` 
  - to use, run  `python3 main.py`.
 
 ## Dependencies
  - [Python 3] to run the program
  - [keras] network definition and backprop
  - [tensorflow] for tensors
- - [dlib] for face detection model
+ - [dlib developers] for face detection model
  - [NumPy] to visualize individual layers
 
    [Combining Residual Networks with LSTMs for Lipreading]: <https://arxiv.org/pdf/1703.04105.pdf>
